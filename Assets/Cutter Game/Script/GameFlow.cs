de78ceba14c02f2529e012c1c97f5d8f2890782c
lamzzzzq <lamzzzzq@gmail.com>
2022-08-22 11:08:44 +0800

    Test

--- Assets/Cutter Game/Script/GameFlow.cs
+++ Assets/Cutter Game/Script/GameFlow.cs
@@ -9,7 +9,7 @@ public class GameFlow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //just a test
     }
 
     // Update is called once per frame
