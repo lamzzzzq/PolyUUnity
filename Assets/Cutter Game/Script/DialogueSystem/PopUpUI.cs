8875b35c1a2fa592d915503577d6dc4b48280fa6
lamzzzzq <lamzzzzq@gmail.com>
2022-08-31 04:55:54 +0800

    regular update

---
+++ Assets/Cutter Game/Script/DialogueSystem/PopUpUI.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopUpUI : MonoBehaviour
+{
+    [Header("UI组件")]
+    public Text textLable;
+    public Button button;
+
+    [Header("文本文件")]
+    public TextAsset textFile;
+
+    public int index;
+
+    List<string> textList = new List<string>();
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        GetTextFromFile(textFile);
+    }
+    private void OnEnable()
+    {
+        textLable.text = textList[index];
+        index++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A)&& index == textList.Count)
+        {
+            gameObject.SetActive(false);
+            index = 0;
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            textLable.text = textList[index];
+            index++;
+        }
+    }
+
+    void GetTextFromFile(TextAsset file)
+    {
+        textList.Clear();
+        index = 0;
+
+        var lineData = file.text.Split('\n');
+
+        foreach (var line in lineData)
+        {
+            textList.Add(line);
+        }
+    }
+}
