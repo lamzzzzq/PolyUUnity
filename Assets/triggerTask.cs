5c368720cde5e736c7d9572e07cd55728d6324af
lamzzzzq <lamzzzzq@gmail.com>
2022-09-22 23:34:53 +0800

    update

--- Assets/triggerTask.cs
+++ Assets/triggerTask.cs
@@ -7,17 +7,20 @@ public class triggerTask : MonoBehaviour
     public GameObject obj1;
     public GameObject obj2;
     public GameObject books;
+    public GameObject file;
+    public GameObject pen;
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Basketball")
+        if(other.gameObject.name == "DropPoint")
         {
             obj1.SetActive(false);
             obj2.SetActive(false) ;
 
             books.SetActive(true);
-
+            file.SetActive(true);
+            pen.SetActive(true);
         }    
        
     }
