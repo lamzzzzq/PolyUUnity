ec120ce60a9895de7a6e53ec23eb2a10497369fd
lamzzzzq <lamzzzzq@gmail.com>
2022-09-23 09:24:43 +0800

    Final Update
    - Update Home Scene UI & Button Logic
    - Fix Score System in Scene 2
    - Fix Scene 2 Quest Logic

---
+++ Assets/Scripts/Level1Score.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PixelCrushers.DialogueSystem;
+using UnityEngine.UI;
+
+public class Level1Score : MonoBehaviour
+{
+    public Text Point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int value = DialogueLua.GetVariable("Point_Level").asInt;
+
+        Point.text = value.ToString();
+    }
+}
