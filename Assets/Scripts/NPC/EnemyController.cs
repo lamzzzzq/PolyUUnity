ecd6396f873dbee6d5003c90357bc6cbcb49c13b
lamzzzzq <lamzzzzq@gmail.com>
2022-09-28 21:59:50 +0800

    Navigation AI Development
    - Auto Navigation
    - Detect Player & Stop, waiting for interaction

--- Assets/Scripts/NPC/EnemyController.cs
+++ Assets/Scripts/NPC/EnemyController.cs
@@ -3,25 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public enum EnemyStates {GUARD, PATROL, CHASE}
+public enum EnemyStates {GUARD, PATROL, READYTOTALK}
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyController : MonoBehaviour
 {
     private EnemyStates enemystates;
 
     private NavMeshAgent agent;
+    private Animator anim;
+
+    public FacePlayer faceplayer;
 
     [Header("Basic Settings")]
     public float sightRadius;
+    public bool isGuard;
+
+    private GameObject attackTarget;
+
+    public float lookAtTime;
+    private float remainLookAtTime;
+
+    [Header("Patrol State")]
+    public float patrolRange;
+
+    private Vector3 wayPoint;
+    private Vector3 guardPos;
+
+    //bool配合動畫
+    bool isWalk;
+    bool isTalk;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        anim = GetComponent<Animator>();
+
+        guardPos = transform.position;
+
+        remainLookAtTime = lookAtTime;
+    }
+
+    private void Start()
+    {
+        if(isGuard)
+        {
+            enemystates = EnemyStates.GUARD;
+        }
+        else
+        {
+            enemystates = EnemyStates.PATROL;
+            GetNewWayPoint();
+        }
     }
 
     private void Update()
     {
         SwitchStates();
+        SwitchAnimation();
+
+    }
+
+    void SwitchAnimation()
+    {
+        anim.SetBool("Walk", isWalk);
+        anim.SetBool("Talk", isTalk);
     }
 
 
@@ -35,7 +81,7 @@ public class EnemyController : MonoBehaviour
         //爲什麽寫在這裏？
         if(FoundPlayer())
         {
-            enemystates = EnemyStates.CHASE;
+            enemystates = EnemyStates.READYTOTALK;
             Debug.Log("找到Player");
         }
 
@@ -46,8 +92,47 @@ public class EnemyController : MonoBehaviour
             case EnemyStates.GUARD:
                 break;
             case EnemyStates.PATROL:
+                {
+                    //判断是否走到了随机巡逻点
+                    if(Vector3.Distance(wayPoint,transform.position)<= agent.stoppingDistance)
+                    {
+                        isWalk = false;
+                        if(remainLookAtTime > 0 )
+                        {
+                            remainLookAtTime -= Time.deltaTime;
+                        }
+                        else 
+                        {
+                            GetNewWayPoint();
+                        }
+                    }
+                    else
+                    {
+                        isWalk = true;
+                        agent.destination = wayPoint;
+                    }
+
+                }
                 break;
-            case EnemyStates.CHASE:
+
+            case EnemyStates.READYTOTALK:
+                //Todo:望向player
+                //TODO:停止移动
+                //TODO：配合動畫
+                if (!FoundPlayer())
+                {
+                    //TODO：拉脫回到上一個狀態
+                    faceplayer.enabled = false;
+                    enemystates = EnemyStates.PATROL;
+                    GetNewWayPoint();
+                }
+                else
+                {
+                    faceplayer.enabled = true;
+                    isWalk = false;
+                    agent.destination = transform.position;
+                }
+
                 break;
         }
     }
@@ -59,13 +144,37 @@ public class EnemyController : MonoBehaviour
 
         foreach(var target in colliders)
         {
-            if(target.CompareTag("Player"))
+            if(target.CompareTag("SubPlayer"))
             {
+                attackTarget = target.gameObject; 
                 return true;
             }
         }
-
+        attackTarget = null;
         return false;
+    }
+
+
+    void GetNewWayPoint()
+    {
+        remainLookAtTime = lookAtTime;
+
+        float randomX = Random.Range(-patrolRange, patrolRange);
+        float randomZ = Random.Range(-patrolRange, patrolRange);
+
+        Vector3 randomPoint = new Vector3(guardPos.x + randomX, transform.position.y, guardPos.z + randomZ);
+
+        //wayPoint = randomPoint;
+
+        NavMeshHit hit;
+        wayPoint = NavMesh.SamplePosition(randomPoint, out hit, patrolRange, 1) ? hit.position : transform.position;
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, sightRadius);
     }
 }
 
