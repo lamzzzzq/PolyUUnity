73ca11bdf73b1a2eb9f7ae9e06a85893e25e72fb
lamzzzzq <lamzzzzq@gmail.com>
2022-10-17 23:31:58 +0800

    Update
    - Sandwich Making (TBC)

---
+++ Assets/Cutter Game/Script/InstantiateAndGrab.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+
+
+public class InstantiateAndGrab : MonoBehaviour
+{
+    public GameObject ObjectToCreate;
+
+    GameObject go;
+    Grabbable grabbable;
+    Grabber grabber;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        grabber = other.GetComponent<Grabber>();
+
+        if(grabber != null && !grabber.HoldingItem)
+        {
+            go = GameObject.Instantiate(ObjectToCreate, grabber.transform.position, grabber.transform.rotation);
+
+            grabbable = go.GetComponent<Grabbable>();
+
+            grabber.GrabGrabbable(grabbable);
+        }
+    }
+
+
+    private void Update()
+    {
+        if((Input.GetKeyDown(KeyCode.J) || InputBridge.Instance.BButtonDown))
+        {
+            grabbable.CanBeDropped = true;
+            grabber.TryRelease();
+        }
+    }
+}
