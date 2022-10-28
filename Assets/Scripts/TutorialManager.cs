8ad2bc6c67b357e5106b3439db965cb4df5317c9
lamzzzzq <lamzzzzq@gmail.com>
2022-10-28 10:32:07 +0800

    Update

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+    private int popUpIndex;
+
+    private void Update()
+    {
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if(i== popUpIndex)
+            {
+                popUps[popUpIndex].SetActive(true);
+            }
+            else
+            {
+                popUps[popUpIndex].SetActive(false);
+            }
+        }
+        if(popUpIndex == 0)
+        {
+            if()
+        }
+    }
+}
