3abd0c4538088e7dd23722660b60831396509ef5
lamzzzzq <lamzzzzq@gmail.com>
2022-11-25 04:41:40 +0800

    Update Level 3

---
+++ Assets/Scripts/NPC/Scene03/BlindPeople.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BlindPeople : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Animator _anim;
+
+    public Transform Target;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, Target.position) < 0.5f)
+        {
+            Debug.Log("Hey0");
+            _agent.isStopped = true;
+
+            Debug.Log("Hey1");
+            _anim.SetBool("Idle", true);
+            _anim.SetBool("Walk", false);
+            Debug.Log("Hey2");
+        }
+
+    }
+    //从外部call这个function 只执行一次
+    public void WalkTowardTheDestination()
+    {
+        _agent.isStopped = false;
+        _agent.SetDestination(Target.transform.position);
+        _anim.SetBool("Walk", true);
+        _anim.SetBool("Idle", false);
+    }
+}
