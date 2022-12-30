22c23c1b2c96c649faaa4a2a548299aaaee2ec3a
lamzzzzq <lamzzzzq@gmail.com>
2022-12-30 10:18:12 +0800

    Update

--- Assets/Scripts/BubbleGun.cs
+++ Assets/Scripts/BubbleGun.cs
@@ -2,25 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using BNG;
 
 public class BubbleGun : MonoBehaviour
 {
-    public UnityEvent onShootEvent;
+    public ParticleSystem particleSystem;
+    public AudioSource audioSource;
+
+    private bool keyPress;
 
     private void Update()
     {
-        if (onShootEvent != null)
+        if(InputBridge.Instance.LeftTriggerDown || InputBridge.Instance.RightTriggerDown)
         {
-            onShootEvent.Invoke();
+            keyPress = true;
+            Debug.Log(keyPress);
+        }
+        if (InputBridge.Instance.LeftTriggerUp || InputBridge.Instance.RightTriggerUp)
+        {
+            keyPress = false;
+            Debug.Log(keyPress);
+        }
+
+        if(keyPress)
+        {
+            particleSystem.Play();
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
-
-    public void Shoot()
-    {
-        if(Input.GetKey(KeyCode.O))
-        {
-            Debug.Log("That's right!");
-        }
-    }
-
 }
---
+++ Assets/Scripts/NPC/Scene04/BallonGirl.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using PixelCrushers.DialogueSystem;
+
+public class BallonGirl : MonoBehaviour
+{
+    public Transform target;
+    public FacePlayer facePlayer;
+    public float WalkValue;
+    public float StopValue;
+
+    private NavMeshAgent _agent;
+    private Animator _anim;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        _agent.SetDestination(target.transform.position);
+
+        if (Vector3.Distance(transform.position, target.position) > WalkValue)
+        {
+            Debug.Log("I am walking");
+            _agent.isStopped = false;
+            _anim.SetBool("Walk", true);
+            facePlayer.enabled = false;
+        }
+
+        if (Vector3.Distance(transform.position, target.position) < StopValue)
+        {
+            Debug.Log("I am stop");
+            _agent.isStopped = true;
+            _anim.SetBool("Walk", false);
+            _anim.SetBool("Fall", false);
+
+            facePlayer.enabled = true;
+        }
+    }
+}
