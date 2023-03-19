f5d38b0ea39be60f59094a552a560edeed0d846f
lamzzzzq <lamzzzzq@gmail.com>
2023-03-19 13:01:45 +0800

    FatherFade

---
+++ Assets/Scripts/Level 1/ScreenFadeFatherDisappear.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BNG;
+
+public class ScreenFadeFatherDisappear : MonoBehaviour
+{
+    public GameObject father;
+    public GameObject block;
+    public ScreenFader fader; //  the screen fader on the center eye camera, drag the camera here
+    //[SerializeField] float fadeInWait = 0.5f;
+
+    public void CallFadeIn()
+    {
+        fader.DoFadeIn();
+    }
+
+    public void CallFadeOut()
+    {
+        fader.DoFadeOut();
+    }
+
+    IEnumerator WaitFadeAndFatherDisappear()
+    {
+        yield return new WaitForSeconds(1);
+        CallFadeIn();
+        Invoke(nameof(DisableFather), 1f);
+    }
+
+
+    public void FadeAndFatherDisappear()
+    {
+        /*        CallFadeIn();
+                Invoke(nameof(DisableFather), 1f);*/
+        StartCoroutine(WaitFadeAndFatherDisappear());
+    }
+
+    public void DisableFather()
+    {
+        father.SetActive(false);
+        block.SetActive(false);
+        CallFadeOut();
+    }
+
+/*    public void FadeCutscene()
+    {
+        fader.DoFadeIn();
+        Invoke(nameof(FadeSecond), 1f);
+
+        Debug.Log("2");
+    }*/
+
+    public void FadeSecond()
+    {
+        CallFadeOut();
+        Debug.Log("1");
+
+    }
+}
